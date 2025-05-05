593c71ce2459291f7fa695cd1ef5496f8fb0b8d5
Adrielle <adriellehamesferreira@outlook.com>
2025-05-05 16:05:22 -0300

    criei exception para testar

--- src/core/testing.php
+++ src/core/testing.php
@@ -5,5 +5,11 @@ include("../models/register.php");
 $obj1 = new Register('name','surname','cpf', 'rg','2020-01-01', '9321921', 'SC', 'saojose', 'bairroas', 'endereco', 390, 'casa', 3000, 'dev', '4831821321', 'C:\32321.jpg', 'adsasd@dass.com', '2025-01-01', 'pending', NULL );
 
 // $resultado = $conexao->fetchQuery('SELECT * FROM users');
+try {
+    
 $obj1->insertDb();
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
 // var_dump($resultado[0]['name']);
