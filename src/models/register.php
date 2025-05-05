c75611a2278561b4991b76441eaa6495cc4c25f0
Adrielle <adriellehamesferreira@outlook.com>
2025-05-05 08:32:18 -0300

    finalizei o arquivo register.php

---
+++ src/models/register.php
@@ -0,0 +1,96 @@
+<?php 
+
+require_once 'core/Database.php'; //importantado o arquivo Database.php
+
+class Register{
+private $id;
+private $name;
+private $surname;
+private $cpf;
+private $rg;
+private $birth_date;
+private $postal_code;
+private $state;
+private $city;
+private $district;
+private $adress;
+private $number;
+private $complement;
+private $monthly_income;
+private $profession; 
+private $telephone;
+private $document_photo;
+private $email;
+private $request_date; 
+private $status_register;
+private $user_complience_approve; 
+private $db; //armazena os dados de conexão
+
+
+private function __construct($id,$name,$surname,$cpf,$rg,$birth_date,
+$postal_code,$state,$city,$district,$adress,$number,$complement,
+$monthly_income,$profession,$telephone,$document_photo,$email,
+$request_date,$status_register,$user_complience_approve){
+
+    $this->id= $id; 
+    $this->name= $name; 
+    $this->surname= $surname;
+    $this->cpf= $cpf;
+    $this->rg= $rg;
+    $this->birth_date= $birth_date;
+    $this->postal_code= $postal_code;
+    $this->state= $state;
+    $this->city= $city;
+    $this->district= $district;
+    $this->adress= $adress;
+    $this->number= $number;
+    $this->complement= $complement;
+    $this->monthly_income= $monthly_income;
+    $this->profession= $profession;
+    $this->telephone= $telephone;
+    $this->document_photo=$document_photo;
+    $this->email=$email;
+    $this->request_date=$request_date;
+    $this->status_register=$status_register;
+    $this->user_complience_approve=$user_complience_approve;
+    $this->db= new Database(); //estabelece a instância de conexão
+ 
+ 
+ 
+ 
+}
+
+ private function insert_db(){// estou usando o "executequery" do  Database.php
+    $query=("INSERT INTO register (id, name, surname, cpf, rg, birth_date, postal_code,state,city,district,adress, number,complement,monthly_income,profession,telephone, document_photo,email,request_date,status_register,user_complience_approve ) VALUES (:id, :name, :surname, :cpf, :rg, :birth_date, :postal_code,:state,:city,:district,:adress, :number,:complement,:monthly_income,:profession,:telephone, :document_photo,:email,:request_date,:status_register,:user_complience_approve");
+    $params= [
+    ':id'=> $this->id,
+    ':name'=> $this->name,
+    ':surname'=> $this->surname,
+    ':cpf'=> $this->cpf,
+    ':birth_date'=> $this->birth_date,
+    ':postal_code'=> $this->postal_code,
+    ':state'=> $this->state,
+    ':city'=> $this->city,
+    ':district'=> $this->district,
+    ':adress'=> $this->adress,
+    ':number'=> $this->number,
+    ':complement'=> $this->complement,
+    ':monthly_income'=> $this->monthly_income,
+    ':profession'=> $this->profession,
+    ':telephone'=> $this->telephone,
+    ':document_photo'=> $this->document_photo,
+    ':email'=> $this->email,
+    ':request_date'=> $this->request_date,
+    ':status_register'=> $this->status_register,
+    ':user_complience_approve'=> $this->user_complience_approve
+];
+
+
+$this->db->executeQuery($query, $params);//executando a inserção de dados
+ }
+
+}
+
+
+
+?>
