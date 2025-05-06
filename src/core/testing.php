4ea4cf5aa8cb23b3a94aa7bee20a1c9063c82683
Adrielle <adriellehamesferreira@outlook.com>
2025-05-06 09:26:06 -0300

    testando inserção no banco de dados

--- src/core/testing.php
+++ src/core/testing.php
@@ -2,12 +2,12 @@
 
 include("../models/register.php");
 
-$obj1 = new Register('name','surname','cpf', 'rg','2020-01-01', '9321921', 'SC', 'saojose', 'bairroas', 'endereco', 390, 'casa', 3000, 'dev', '4831821321', 'C:\32321.jpg', 'adsasd@dass.com', '2025-01-01', 'pending', NULL );
+$user = new Register('name','surname','cpf', 'rg','2000-01-01', '9321921', 'SC', 'saojose', 'bairroas', 'endereco', 390, 'casa', 3000, 'dev', '4831821321', 'C:\32321.jpg', 'adsasd@dass.com', '2025-01-01', 'pending', NULL );
 
 // $resultado = $conexao->fetchQuery('SELECT * FROM users');
 try {
     
-$obj1->insertDb();
+$user->insertDb();
 }catch(Exception $e){
     echo $e->getMessage();
 }
