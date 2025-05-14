5d2068fc9c713e1e639897640e1c4df4b6685834
Adrielle <adriellehamesferreira@outlook.com>
2025-05-14 08:21:19 -0300

    Defini as rotas no arquivo index.php

--- index.php
+++ index.php
@@ -9,5 +9,17 @@ Autoloader::register();
 
 // Definindo as rotas
 Routes::add('/bank-me', 'Controllers\HomeController', 'index');
+Routes::add('/bank-me/Register', 'Controllers\RegisterController', 'index');
+Routes::add('/bank-me/Feactures', 'Controllers\FeacturesController', 'index');
+Routes::add('/bank-me/Integration', 'Controllers\IntegrationController', 'index');
+Routes::add('/bank-me/Login', 'Controllers\LoginController', 'index');
+Routes::add('/bank-me/Products', 'Controllers\ProductsController', 'index');
+Routes::add('/bank-me/Resources', 'Controllers\ResourcesController', 'index');
+
+
+
+
+
+
 // Despachando a rota
 Routes::dispatch();
